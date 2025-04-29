30bb2487d0bbe11203e180c2407772f276867b3e
webmucidi <fatihcelik1907@gmail.com>
2025-04-29 09:32:33 +0300

    Add project files.

---
+++ StudentApp/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+    }
+}
